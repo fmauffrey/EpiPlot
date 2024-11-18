3787412c51551c766a6db118860181999fccb9af
fmauffrey <mauffreyflorian@gmail.com>
2024-11-18 14:32:18 +0100

    Add function to install all required packages

---
+++ install_packages.R
@@ -0,0 +1,7 @@
+install.packages(c(
+  "shiny", "shinyalert", "shinyWidgets", "dplyr", "ggplot2", "forcats",
+  "svglite", "shinydashboard", "plotly", "shinycssloaders", "lubridate",
+  "GGally", "network", "sna", "scales", "readxl", "plotrix", "visNetwork",
+  "RColorBrewer", "stringr", "IRanges", "shinymanager", "shinyjs", 
+  "rmarkdown", "webshot", "tidyr", "here", "shinythemes"
+))
