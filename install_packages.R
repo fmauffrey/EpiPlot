098523ddca70f0b05ee7ddd713fbf9d7d8b2c335
fmauffrey <mauffreyflorian@gmail.com>
2024-11-20 11:27:09 +0100

    Update with all necessary packages

--- install_packages.R
+++ install_packages.R
@@ -1,7 +1,11 @@
-install.packages(c(
+list.of.packages <- c(
   "shiny", "shinyalert", "shinyWidgets", "dplyr", "ggplot2", "forcats",
   "svglite", "shinydashboard", "plotly", "shinycssloaders", "lubridate",
   "GGally", "network", "sna", "scales", "readxl", "plotrix", "visNetwork",
   "RColorBrewer", "stringr", "IRanges", "shinymanager", "shinyjs", 
-  "rmarkdown", "webshot", "tidyr", "here", "shinythemes"
-))
+  "rmarkdown", "webshot", "tidyr", "here", "shinythemes", "BiocManager"
+)
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+BiocManager::install("IRanges")
