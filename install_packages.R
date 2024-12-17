f5352bfde0aff961aa5bd4fa12bf262eabfe6d20
fmauffrey <mauffreyflorian@gmail.com>
2024-12-17 16:41:21 +0100

    Move reticulate install to the list of packages and add comments

--- install_packages.R
+++ install_packages.R
@@ -1,16 +1,18 @@
+# First block to execute
 list.of.packages <- c(
   "shiny", "shinyalert", "shinyWidgets", "dplyr", "ggplot2", "forcats",
   "svglite", "shinydashboard", "plotly", "shinycssloaders", "lubridate",
   "GGally", "network", "sna", "scales", "readxl", "plotrix", "visNetwork",
   "RColorBrewer", "stringr", "IRanges", "shinymanager", "shinyjs", 
-  "rmarkdown", "webshot", "tidyr", "here", "shinythemes", "BiocManager", "digest"
+  "rmarkdown", "webshot", "tidyr", "here", "shinythemes", "BiocManager", "digest",
+  "reticulate"
 )
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
 BiocManager::install("IRanges")
 
-install.packages('reticulate')
+# Second block to execute
 reticulate::install_miniconda()
 reticulate::conda_install('r-reticulate', 'python-kaleido')
 reticulate::conda_install('r-reticulate', 'plotly', channel = 'plotly')
